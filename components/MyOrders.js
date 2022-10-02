f7cd0af197bd374306ab229e11b513db18a1d372
Aravinth <aravintht302@gmail.com>
2022-10-02 16:29:00 +0530

    grid table new design

--- components/MyOrders.js
+++ components/MyOrders.js
@@ -1,4 +1,4 @@
-import React, {Fragment, useEffect, useState} from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import styles from "scss/components/MyOrders.module.scss";
 import toast from "./Toast";
 import { useSelector, useDispatch } from "react-redux";
@@ -11,8 +11,8 @@ import ModalPopup from "./ModalPopup";
 
 function MyOrders() {
     const dispatch = useDispatch();
-    const user = useSelector((state)=> state.authState);
-    const { orders } = useSelector((state)=> state.ordersState);
+    const user = useSelector((state) => state.authState);
+    const { orders } = useSelector((state) => state.ordersState);
 
     const [show, setShow] = useState(false);
     const [showModal, setShowModal] = useState(false);
@@ -26,12 +26,12 @@ function MyOrders() {
             headers: {
                 Authorization: `bearer ${user.token}`,
             }
-        }).then(({data})=> {
-            dispatch(ordersState({orders: data}));
+        }).then(({ data }) => {
+            dispatch(ordersState({ orders: data }));
         });
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         if (user.id) {
             getOrders();
         }
@@ -40,11 +40,11 @@ function MyOrders() {
     const submitOrder = (fields, id) => {
         dispatch(toggleLoaderState(true));
         fields.updatedBy = user.id;
-        axios.patch(`/api/orders/${id}`, fields,{
+        axios.patch(`/api/orders/${id}`, fields, {
             headers: {
                 Authorization: `bearer ${user.token}`,
             }
-        }).then(()=>{
+        }).then(() => {
             dispatch(toggleLoaderState(false));
             getOrders();
         })
@@ -71,19 +71,19 @@ function MyOrders() {
         } else if (e.target.value === 'Completed' && row.original.serviceName === 'Audit') {
             setShowModal(true);
             setSelectedRow(row.original);
-        } else{
+        } else {
             const fields = {
                 status: e.target.value
             }
             submitOrder(fields, row.original._id);
         }
-        
+
     }
 
     const columns = React.useMemo(() => [
         {
             Header: 'S No',
-            Cell: (cellObj) => cellObj.row.index+1
+            Cell: (cellObj) => cellObj.row.index + 1
         },
         {
             Header: 'Username',
@@ -112,15 +112,16 @@ function MyOrders() {
         {
             Header: 'Status',
             accessor: 'status',
-            ...(user.isAdmin) ? {Cell: (cellObj) => {
-                return (
-                    <select id="lang" onChange={(e) => handleClickEditRow(e, cellObj.row)} value={cellObj.row.values.status} className="p-2 rounded">
-                        <option value="Pending">Pending</option>
-                        <option value="Approved">Approved</option>
-                        <option value="Completed">Completed</option>
-                    </select>
-                )
-            }
+            ...(user.isAdmin) ? {
+                Cell: (cellObj) => {
+                    return (
+                        <select id="lang" onChange={(e) => handleClickEditRow(e, cellObj.row)} value={cellObj.row.values.status} className="p-2 rounded">
+                            <option value="Pending">Pending</option>
+                            <option value="Approved">Approved</option>
+                            <option value="Completed">Completed</option>
+                        </select>
+                    )
+                }
             } : {}
         },
     ], [user]);
@@ -135,48 +136,51 @@ function MyOrders() {
 
     return (
         <Fragment>
-            <h3 className={styles.heading}>My Orders</h3>
+            <h3 className={`${styles.heading} text-4xl font-bold mb-1 black`}>My <span className="yellow">Orders</span></h3>
+            <p className="text-base font-normal black mb-6">
+                Welcome back! Take a look at your profile here.
+            </p>
             <div className={styles.tableContainer}>
                 <table {...getTableProps()} className="min-w-full leading-normal">
                     <thead>
                         {headerGroups.map((headerGroup, headerIndex) => (
                             <tr {...headerGroup.getHeaderGroupProps()} key={headerIndex}>
-                            {headerGroup.headers.map((column, index) => (
-                            <th
-                                {...column.getHeaderProps()}
-                                className="px-5 py-3 border-b-2 border-yellow-200 bg-yellow-500 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider"
-                                key={index}
-                            >
-                                {column.render('Header')}
-                            </th>
-                            ))}
-                        </tr>
+                                {headerGroup.headers.map((column, index) => (
+                                    <th
+                                        {...column.getHeaderProps()}
+                                        className="px-5 py-3 border-b-2  text-left text-xs font-bold text-gray-700 uppercase tracking-wider"
+                                        key={index}
+                                    >
+                                        {column.render('Header')}
+                                    </th>
+                                ))}
+                            </tr>
                         ))}
                     </thead>
                     <tbody {...getTableBodyProps()}>
-                        {rows.map((row,i) => {
-                        prepareRow(row)
-                        return (
-                            <tr {...row.getRowProps()} key={i}>
-                            {row.cells.map((cell, cellIndex) => {
-                                return (
-                                <td
-                                    {...cell.getCellProps()}
-                                    className="px-5 py-5 border-b border-yellow-200 bg-yellow-100 text-sm"
-                                    key={cellIndex}
-                                >
-                                    {cell.render('Cell')}
-                                </td>
-                                )
-                            })}
-                            </tr>
-                        )
+                        {rows.map((row, i) => {
+                            prepareRow(row)
+                            return (
+                                <tr {...row.getRowProps()} key={i}>
+                                    {row.cells.map((cell, cellIndex) => {
+                                        return (
+                                            <td
+                                                {...cell.getCellProps()}
+                                                className="px-5 py-5 border-b  text-sm"
+                                                key={cellIndex}
+                                            >
+                                                {cell.render('Cell')}
+                                            </td>
+                                        )
+                                    })}
+                                </tr>
+                            )
                         })}
                     </tbody>
                 </table>
             </div>
-            {show && <RevenuePopup selectedRow={selectedRow} setShowPopup={setShow} submitHandler={submitHandler}/>}
-            {showModal && <ModalPopup selectedRow={selectedRow} setShowPopup={setShowModal} submitHandler={submitHandler}/>}
+            {show && <RevenuePopup selectedRow={selectedRow} setShowPopup={setShow} submitHandler={submitHandler} />}
+            {showModal && <ModalPopup selectedRow={selectedRow} setShowPopup={setShowModal} submitHandler={submitHandler} />}
         </Fragment>
     );
 }
